a2c8dca5ddfebbd4f8f07a1a0392c2c0a8a6cda3
GeoFox-bingyan <wangkai_geo@163.com>
2019-01-21 17:31:52 +0800

    Initial Commit

---
+++ MagRadar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagRadar
+//
+//  Created by Mac-wk on 2019/1/21.
+//  Copyright © 2019 Mac-wk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
